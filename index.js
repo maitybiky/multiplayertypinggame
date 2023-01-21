abbd2418ba8bc13da0cc6c022df97c7f09b0f796
surajit <bikymaity489@gmail.com>
2023-01-21 23:30:47 +0530

    first

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const { Server } = require("socket.io");
+const PORT = 4000;
+let users;
+let result;
+const app = express();
+const server = http.createServer(app);
+// const io = socketio(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+io.on("connection", (socket) => {
+  console.log("connected +" + socket.id);
+  socket.emit("joined-user", users);
+  socket.on("join_room", (data) => {
+    users = data.location;
+    console.log(users, "kk");
+    socket.broadcast.emit("join-alert", data);
+  });
+  socket.on("ready", () => {
+    socket.broadcast.emit("opponent_ready");
+  });
+  socket.on("typing", (typeData) => {
+    // console.log("typeData", typeData);
+    socket.broadcast.emit("remoteType", typeData);
+    // io.to(userData.room).emit("location_up", locData);
+  });
+  socket.on("result", (resultData) => {
+    result = resultData;
+    console.log(result)
+    socket.broadcast.emit("remoteResult", resultData);
+    result = null;
+    // io.to(userData.room).emit("location_up", locData);
+  });
+
+  socket.on("disconnect", (socket) => {
+    users = {};
+    //  socket.broadcast.emit("userLeft");
+    console.log("connected -");
+  });
+});
+
+server.listen(PORT, "0.0.0.0", () =>
+  console.log(`server running on port ${PORT}`)
+);
